34d902356d5db82e6fad7b56c4c13d568f4ce513
MANISHA <manishapanchal169@gmail.com>
2022-05-31 11:33:43 +0530

    fix bug

--- src/Components/TakeNote/TakeNote.js
+++ src/Components/TakeNote/TakeNote.js
@@ -1,12 +1,13 @@
 import { useAtom } from 'jotai';
 import React from 'react';
-import { inputText, inputTitle } from '../../globalAtom';
+import { inputText, inputTitle, otherNote, pinNote } from '../../globalAtom';
 import './TakeNote.css';
 
 const TakeNote = () => {
 	const [titleVal, setTitleVal] = useAtom(inputTitle);
 	const [textVal, setTextVal] = useAtom(inputText);
-
+	const [othersNotes, setOthersNote] = useAtom(otherNote);
+	const [pinnedNotes, setPinnedNotes] = useAtom(pinNote);
 	const changeTitleInputVal = (event) => {
 		setTitleVal(event.target.value);
 	};
@@ -14,22 +15,41 @@ const TakeNote = () => {
 	const changeTextInputVal = (event) => {
 		setTextVal(event.target.value);
 	};
+	const onSaveNote = () => {
+		const x = { title: titleVal, text: textVal };
+		console.log(x);
+		console.log(othersNotes);
+		setOthersNote([x, ...othersNotes]);
+	};
+	const onPinNote = () => {
+		console.log('pinneed');
+		const y = { title: titleVal, text: textVal };
+		setPinnedNotes([y, ...pinnedNotes]);
+	};
 	return (
 		<>
 			<div className='takeNoteWrapper'>
-				<input
-					className='inputBox titleInputBox'
-					value={titleVal}
-					onChange={changeTitleInputVal}
-					autoFocus
-				/>
-				<textarea
-					rows='6'
-					cols='46'
-					className='inputBox noteInputBox'
-					value={textVal}
-					onChange={changeTextInputVal}
-				/>
+				<div>
+					<input
+						placeholder='Title'
+						className='inputBox titleInputBox'
+						value={titleVal}
+						onChange={changeTitleInputVal}
+						autoFocus
+					/>
+					<textarea
+						placeholder='Make a note...'
+						rows='6'
+						cols='46'
+						className='inputBox noteInputBox'
+						value={textVal}
+						onChange={changeTextInputVal}
+					/>
+				</div>
+				<div>
+					<button onClick={onSaveNote}>Add Note</button>
+					<button onClick={onPinNote}>Pin Note</button>
+				</div>
 			</div>
 		</>
 	);

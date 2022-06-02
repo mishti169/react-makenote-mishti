e3e99e718c668ac2a1fba090d74c7f6c34951a20
MANISHA <manishapanchal169@gmail.com>
2022-06-02 18:20:55 +0530

    add header look

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
 	return (
-		<>
-			<div>
-				<h3>Make Note</h3>
-				<div></div>
+		<div className='headerWrapper'>
+			<div className='notePenWrapper'>
+				<div className='titleIconWrapper'>
+					<h3>Make Note</h3>
+					<span class='material-symbols-outlined '>edit_note</span>
+				</div>
 			</div>
-		</>
+			<a href='https://github.com/mishti169' className='headerLink'>
+				Git Hub
+			</a>
+		</div>
 	);
 };
 

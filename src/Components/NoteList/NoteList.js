1a778b541f7a47cfabbf41f526137e0fbf2e1de6
MANISHA <manishapanchal169@gmail.com>
2022-06-05 14:36:31 +0530

    Revert "do not render ,if note is empty"
    This reverts commit f37096c44f0f072ae60b2b9867caaea4d6aee179.

--- src/Components/NoteList/NoteList.js
+++ src/Components/NoteList/NoteList.js
@@ -2,14 +2,11 @@ import React from 'react';
 import NoteItem from '../NoteItem/NoteItem';
 import './NoteList.css';
 const NoteList = (props) => {
-	const { notes, title } = props;
-	if (!notes.length) return null;
-
 	return (
 		<>
-			<h2>{title}</h2>
+			<h2>{props.title}</h2>
 			<div className='notesWrapper '>
-				{notes.map(function (currVal) {
+				{props.notes.map(function (currVal) {
 					return (
 						<NoteItem
 							key={currVal.id}

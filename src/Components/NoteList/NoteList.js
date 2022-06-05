18102bcf0a6f7fd421742bc60233e5d0e62262d3
MANISHA <manishapanchal169@gmail.com>
2022-06-05 17:23:49 +0530

    add delete all btn on NL

--- src/Components/NoteList/NoteList.js
+++ src/Components/NoteList/NoteList.js
@@ -1,10 +1,30 @@
+import { useAtom } from 'jotai';
 import React from 'react';
+import { otherNote, pinNote } from '../../globalAtom';
 import NoteItem from '../NoteItem/NoteItem';
 import './NoteList.css';
+
 const NoteList = (props) => {
+	const [, setPinnedNotes] = useAtom(pinNote);
+	const [, setOthersNote] = useAtom(otherNote);
+
+	const onDelAll = () => {
+		if (props.title === 'Pinned') {
+			setPinnedNotes('');
+		} else {
+			setOthersNote('');
+		}
+	};
+
 	return (
 		<>
-			<h2>{props.title}</h2>
+			<div className='nIHeadingContainer'>
+				<h2>{props.title}</h2>
+				<button className='border borderRadius' onClick={onDelAll}>
+					<i className='fa-solid fa-trash iconColor mr10 '></i>
+					Delete All
+				</button>
+			</div>
 			<div className='notesWrapper '>
 				{props.notes.map(function (currVal) {
 					return (
